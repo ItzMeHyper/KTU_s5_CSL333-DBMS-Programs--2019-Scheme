b135492e096f4ee94a24ff9fa7b561951f86ead7
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2025-10-22 00:20:24 +0530

    Update Exp 13.sql

--- EXP 13/Exp 13.sql
+++ EXP 13/Exp 13.sql
@@ -1,80 +1,122 @@
 SET SERVEROUTPUT ON;
-SHOW SERVEROUTPUT;
+SHOW SERVEROUTPUT; 
 
---Factorial
-DECLARE
-  num INT;
-  i INT;
-BEGIN
-  num := :num;
-  i := num - 1;
-  
-  WHILE i > 0 LOOP
-    num := num * i;
-    i := i - 1;
-  END LOOP;
-  
-  dbms_output.put_line('Factorial is ' || num);
-END;
-/
---Greatest Of Three Numbers
-SET SERVEROUTPUT ON;
-SHOW SERVEROUTPUT;
+--1
 
 DECLARE
-    numA INT;
-    numB INT;
-    numC INT;
+    num INT;
+    i   INT;
 BEGIN
-    numA := :numA;
-    numB := :numB;
-    numC := :numC;
+    -- Assign value to num from a bind variable
+    num := :num;  
+    i := num - 1;
 
-    IF (numA > numB AND numA > numC) THEN
-        dbms_output.put_line(numA || ' is greatest');
-    ELSIF (numB > numA AND numB > numC) THEN
-        dbms_output.put_line(numB || ' is greatest');
+    IF num = 0 THEN
+        dbms_output.put_line('Factorial is: 1');
     ELSE
-        dbms_output.put_line(numC || ' is greatest');
+        WHILE i > 0 LOOP
+            num := num * i;
+            i := i - 1;
+        END LOOP;
+        dbms_output.put_line('Factorial is: ' || num);
     END IF;
 END;
 /
---Calculator
+
+---2
+
 SET SERVEROUTPUT ON;
-SHOW SERVEROUTPUT;
+SHOW SERVEROUTPUT; 
 
 DECLARE
     numa INT;
     numb INT;
-    operatorof INT;
-    sumOf INT;
-    mul INT;
-    sub INT;
-    div INT;
+    numc INT;
 BEGIN
-    numa := numa;
-    numb := numb;
-    sumOf := numa + numb;
-    sub := numa - numb;
-    mul := numa * numb;
-    
-    IF (numb != 0) THEN
+    numa := :numa;
+    numb := :numb;
+    numc := :numc;
+
+    IF (numa > numb AND numa > numc) THEN
+        dbms_output.put_line(numa || ' is the greatest');
+    ELSIF (numb > numa AND numb > numc) THEN
+        dbms_output.put_line(numb || ' is the greatest');
+    ELSE
+        dbms_output.put_line(numc || ' is the greatest');
+    END IF;
+END;
+/
+
+--3
+
+SET SERVEROUTPUT ON;
+SHOW SERVEROUTPUT;
+
+DECLARE
+    numa        INT;
+    numb        INT;
+    operatorof  INT;
+    sumof       INT;
+    mul         INT;
+    sub         INT;
+    div         NUMBER;
+BEGIN
+    numa := :numa;
+    numb := :numb;
+    operatorof := :operatorof;
+
+    sumof := numa + numb;
+    sub   := numa - numb;
+    mul   := numa * numb;
+
+    IF numb != 0 THEN
         div := numa / numb;
     END IF;
-    
-    operatorof := operatorof;
-    
+
     CASE operatorof
-        WHEN 1 THEN 
-            dbms_output.put_line('Sum is ' || sumOf);
-        WHEN 2 THEN 
+        WHEN 1 THEN
+            dbms_output.put_line('Sum is ' || sumof);
+        WHEN 2 THEN
             dbms_output.put_line('Difference is ' || sub);
-        WHEN 3 THEN 
+        WHEN 3 THEN
             dbms_output.put_line('Product is ' || mul);
-        WHEN 4 THEN 
+        WHEN 4 THEN
             dbms_output.put_line('Quotient is ' || div);
-        ELSE 
+        ELSE
             dbms_output.put_line('Enter correct choice');
     END CASE;
 END;
 /
+
+--4
+
+SET SERVEROUTPUT ON;
+SHOW SERVEROUTPUT;
+
+DECLARE
+    a   INT;
+    b   INT;
+    c   INT;
+    num INT;
+BEGIN
+    a := 0;
+    b := 1;
+    c := 0;
+    num := &num;  -- input from user
+
+    num := num - 2;
+    dbms_output.put_line('The Fibonacci series is:');
+    dbms_output.put_line(a);
+    dbms_output.put_line(b);
+
+    WHILE (num > 0)
+    LOOP
+        c := a + b;
+        a := b;
+        b := c;
+        dbms_output.put_line(c);
+        num := num - 1;
+    END LOOP;
+END;
+/
+
